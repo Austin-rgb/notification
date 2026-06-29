9125174ddc991c9fd557ac6160597382096ae8f4
Austin-rgb <prof.odhiambo.ostine@gmail.com>
2026-06-30 00:59:16 +0300

    added tags endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
-use actix_web::web;
-use actix_web::web::ServiceConfig;
+use actix_web::{web,HttpResponse, web::ServiceConfig, post, Responder};
 use actixutils::{Identity, Validate};
 use anyhow::Result;
 use emailgrid::EmailingContext;
-use mgk::{Module as Mgk, Sender, IdResolver};
-use push::{NotificationRequest,Config};
-use serde::{Deserialize, Serialize};
+use mgk::{IdResolver, Module as Mgk, Sender};
+use push::{Config, NotificationRequest};
+use sqlx::SqlitePool;
 use sqlx::{Pool, Sqlite};
 use std::env;
 use std::sync::Arc;
@@ -15,19 +14,73 @@ struct Push(Config);
 struct Console;
 struct Email(EmailingContext);
 
-struct MyIdResolver;
+pub struct MyIdResolver {
+    pool: SqlitePool,
+}
+
+impl MyIdResolver {
+    pub fn new(pool: SqlitePool) -> Self {
+        Self { pool }
+    }
+}
 
 #[async_trait::async_trait]
-impl IdResolver for MyIdResolver{
-    async fn resolve(&self, id:Identifier)->Result<Uuid>{
-        Ok(Uuid::new_v4())
+impl IdResolver for MyIdResolver {
+    async fn resolve(&self, id: Identifier) -> Result<Uuid> {
+        match id {
+            Identifier::Uuid(id) => Ok(id),
+
+            Identifier::Tag(tag) => {
+                let user_id: Uuid =
+                    sqlx::query_scalar("SELECT user_id FROM notification_tags WHERE tag = ?")
+                        .bind(tag)
+                        .fetch_one(&self.pool)
+                        .await?;
+
+                Ok(user_id)
+            }
+        }
+    }
+}
+
+#[derive(serde::Deserialize)]
+pub struct RegisterTagRequest {
+    pub tag: String,
+    pub user_id: Uuid,
+}
+
+#[post("/tags")]
+async fn register_tag(
+    pool: web::Data<SqlitePool>,
+    req: web::Json<RegisterTagRequest>,
+) -> impl Responder {
+    match sqlx::query(
+        r#"
+        INSERT INTO notification_tags(tag, user_id)
+        VALUES(?, ?)
+        ON CONFLICT(tag)
+        DO UPDATE SET user_id = excluded.user_id
+        "#,
+    )
+    .bind(&req.tag)
+    .bind(req.user_id)
+    .execute(pool.get_ref())
+    .await{
+        Ok(_)=>HttpResponse::Ok().finish(),
+        Err(e)=>{
+            tracing::error!("error in inserting tag: {e}");
+            HttpResponse::InternalServerError().finish()
+        }
     }
 }
 
 #[async_trait::async_trait]
 impl Sender for Push {
     async fn send(&self, address: String, _subject: String, message: String) -> Result<()> {
-        let notification = NotificationRequest{message, targets: vec![address]};
+        let notification = NotificationRequest {
+            message,
+            targets: vec![address],
+        };
         self.0.push("push".to_string(), notification);
         Ok(())
     }
@@ -65,21 +118,10 @@ pub struct Module {
     push_mgk: Mgk,
     push_: Config,
     console: Mgk,
+    pool: Pool<Sqlite>,
 }
-use chrono::{DateTime, Utc};
-use uuid::Uuid;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Event {
-    pub event_id: Uuid,
-    pub event_version: String,
-    pub occurred_at: DateTime<Utc>,
-    pub producer: String,
-    pub correlation_id: Option<Uuid>,
-    pub trace_id: Option<Uuid>,
-    pub user_id: Option<Uuid>,
-    pub session_id: Option<Uuid>,
-}
+use uuid::Uuid;
 
 fn get_list(name: &str) -> Vec<String> {
     env::var(name)
@@ -99,11 +141,12 @@ impl Module {
         let email_subjects = get_list("email.subjects");
         let push_subjects = get_list("push.subjects");
         let console_subjects = get_list("console.subjects");
-        let idres = Arc::new(MyIdResolver);
+        let idres = Arc::new(MyIdResolver::new(pool.clone()));
         let console = Mgk::new(
             pool.clone(),
             es.clone(),
-            Arc::new(Console {}),idres.clone(),
+            Arc::new(Console {}),
+            idres.clone(),
             console_subjects,
         )
         .await?;
@@ -111,14 +154,16 @@ impl Module {
         let push_mgk = Mgk::new(
             pool.clone(),
             es.clone(),
-            Arc::new(Push(push_.clone())),idres.clone(),
+            Arc::new(Push(push_.clone())),
+            idres.clone(),
             push_subjects,
         )
         .await?;
         let email = Mgk::new(
             pool.clone(),
             es.clone(),
-            Arc::new(Email(emailer)) as Arc<dyn Sender>,idres.clone(),
+            Arc::new(Email(emailer)) as Arc<dyn Sender>,
+            idres.clone(),
             email_subjects,
         )
         .await?;
@@ -128,16 +173,19 @@ impl Module {
             push_mgk,
             push_,
             console,
+            pool,
         })
     }
 
     pub fn config(&self, cfg: &mut ServiceConfig, namespace: &str) {
-        cfg.service(
-            web::scope(namespace)
-                .configure(|cfg| self.push_.config(cfg, "/ws"))
-                .configure(|cfg| self.emailer.config(cfg, "/email"))
-                .configure(|cfg| self.push_mgk.config(cfg, "/push"))
-                .configure(|cfg| self.console.config(cfg, "/console")),
-        );
+        cfg.app_data(web::Data::new(self.pool.clone()))
+            .service(register_tag)
+            .service(
+                web::scope(namespace)
+                    .configure(|cfg| self.push_.config(cfg, "/ws"))
+                    .configure(|cfg| self.emailer.config(cfg, "/email"))
+                    .configure(|cfg| self.push_mgk.config(cfg, "/push"))
+                    .configure(|cfg| self.console.config(cfg, "/console")),
+            );
     }
 }
